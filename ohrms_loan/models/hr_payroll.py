a60c52e74531f3c4e0703991cde5145275ef038d
Niyas Raphy <niyas@cybrosys.in>
2018-11-14 10:00:06 +0530

    [FIX] Payslip and Loan Issue

--- ohrms_loan/models/hr_payroll.py
+++ ohrms_loan/models/hr_payroll.py
@@ -24,7 +24,7 @@ class HrPayslip(models.Model):
         date_to = self.date_to
         contract_ids = []
 
-        ttyme = datetime.fromtimestamp(time.mktime(time.strptime(date_from, "%Y-%m-%d")))
+        ttyme = datetime.fromtimestamp(time.mktime(time.strptime(str(date_from), "%Y-%m-%d")))
         locale = self.env.context.get('lang') or 'en_US'
         self.name = _('Salary Slip of %s for %s') % (
         employee.name, tools.ustr(babel.dates.format_date(date=ttyme, format='MMMM-y', locale=locale)))
