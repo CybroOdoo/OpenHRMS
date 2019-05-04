594a3f6cd88cecb5dc40addc1c2d4b0bd1cef8d1
Ajmalcybrosys <ajmal@cybrosys.in>
2019-05-04 16:53:23 +0530

    [CHG] Manifest Changed

--- hr_theme/__manifest__.py
+++ hr_theme/__manifest__.py
@@ -35,7 +35,7 @@
         'views/open_hrms_theme.xml',
     ],
     'demo': [],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
