c4fae5187ac3afa6cbe161f6a07ee78a7c9c0953
Tintuk <tintuk@cybrosys.in>
2020-02-25 19:36:15 +0530

    [UPDT] styles removed company field in contract, help,arabic

--- hr_multi_company/models/hr_multi_company.py
+++ hr_multi_company/models/hr_multi_company.py
@@ -26,26 +26,26 @@ from odoo import models, fields
 class HrAttendanceMultiCompany(models.Model):
     _inherit = 'hr.attendance'
 
-    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True,
+    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True, help="Company",
                                  default=lambda self: self.env.user.company_id)
 
 
 class HrLeaveMultiCompany(models.Model):
     _inherit = 'hr.leave'
 
-    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True,
+    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True, help="Company",
                                  default=lambda self: self.env.user.company_id)
 
 
 class HrPayslipMultiCompany(models.Model):
     _inherit = 'hr.payslip.run'
 
-    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True,
+    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True, help="Company",
                                  default=lambda self: self.env.user.company_id)
 
 
 class HrSalaryCategoryMultiCompany(models.Model):
     _inherit = 'hr.salary.rule.category'
 
-    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True,
+    company_id = fields.Many2one('res.company', 'Company', copy=False, readonly=True, help="Comapny",
                                  default=lambda self: self.env.user.company_id)
