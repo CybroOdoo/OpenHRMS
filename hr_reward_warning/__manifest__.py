f46b945ef21898609a813425ee5765be11bb7935
Ajmal JK <ajmal@cybrosys.in>
2020-04-18 12:35:57 +0530

    [UPDT] Index Updated 'hr_reward_warning'

--- hr_reward_warning/__manifest__.py
+++ hr_reward_warning/__manifest__.py
@@ -36,7 +36,7 @@
         'views/hr_announcement_view.xml',
     ],
     'demo': ['data/demo_data.xml'],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
