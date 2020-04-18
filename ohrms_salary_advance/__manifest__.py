d3a34d27c97bb7e58a1094a8301c7fb3e65d9fcb
Ajmal JK <ajmal@cybrosys.in>
2020-04-18 12:58:55 +0530

    [UPDT] Index Updated 'ohrms_salary_advance'

--- ohrms_salary_advance/__manifest__.py
+++ ohrms_salary_advance/__manifest__.py
@@ -42,7 +42,7 @@
         'views/salary_advance.xml',
     ],
     'demo': [],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
