e421f13a7fd1ccf1e3ebe02b13b56a63feb037ce
Ajmal JK <ajmal@cybrosys.in>
2020-04-18 12:05:00 +0530

    [UPDT] Index Updated 'hr_employee_updation'

--- hr_employee_updation/__manifest__.py
+++ hr_employee_updation/__manifest__.py
@@ -39,7 +39,7 @@
         'views/hr_notification.xml',
     ],
     'demo': [],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
