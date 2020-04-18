b74d8c997b94dae5b761e01897403cc1bad76284
Ajmal JK <ajmal@cybrosys.in>
2020-04-18 12:24:32 +0530

    [UPDT] Index Updated 'hr_leave_request_aliasing'

--- hr_leave_request_aliasing/__manifest__.py
+++ hr_leave_request_aliasing/__manifest__.py
@@ -37,7 +37,7 @@
         'views/res_config_views.xml',
     ],
     'demo': [],
-    'images': ['static/description/banner.jpg'],
+    'images': ['static/description/banner.png'],
     'license': 'AGPL-3',
     'installable': True,
     'auto_install': False,
