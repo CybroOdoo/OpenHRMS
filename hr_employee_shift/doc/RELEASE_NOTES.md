76a62ce2be983bed0a24b6803ed0b335ef561caa
Ajmal Cybro <ajmal@cybrosys.in>
2021-12-06 16:11:37 +0530

    [ADD] Initial Commit

---
+++ hr_employee_shift/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <hr_employee_shift>
+
+#### 15.10.2020
+#### Version 15.0.1.0.0
+##### ADD
+- Initial commit for OpenHrms Project
---
+++ hr_employee_shift/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import hr_employee_shift, hr_employee_contract, hr_generate_shift, hr_shift_payroll
+# from . import resource
