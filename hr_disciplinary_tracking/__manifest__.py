d8c493a5324122cb5e6e29a30dcc8b0130c06ed1
AjmalCybro <ajmal@cybrosys.in>
2022-06-14 11:25:39 +0530

    [UPDT] Sequence issue updated 'hr_disciplinary_tracking'

--- hr_disciplinary_tracking/__manifest__.py
+++ hr_disciplinary_tracking/__manifest__.py
@@ -1,27 +1,6 @@
-# -*- coding: utf-8 -*-
-###################################################################################
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2021-TODAY Cybrosys Technologies(<https://www.cybrosys.com>).
-#    Author: Ajmal JK(<https://www.cybrosys.com>)
-#
-#    This program is free software: you can modify
-#    it under the terms of the GNU Affero General Public License (AGPL) as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-###################################################################################
 {
-    'name': 'Open HRMS Disciplinary Tracking',
-    'version': '15.0.1.0.0',
+    'name': 'Enter Price Open HRMS Disciplinary Tracking',
+    'version': '15.0.1.0.1',
     'summary': """Employee Disciplinary Tracking Management""",
     'live_test_url': 'https://youtu.be/LFuw2iY4Deg',
     'author': 'Cybrosys Techno solutions,Open HRMS',
@@ -29,7 +8,7 @@
     'maintainer': 'Cybrosys Techno Solutions',
     'category': 'Generic Modules/Human Resources',
     'website': "https://www.openhrms.com",
-    'depends': ['base', 'mail', 'oh_employee_creation_from_user'],
+    'depends': ['base', 'mail', ],
     'data': ['views/disciplinary_action.xml',
              'views/disciplinary_sequence.xml',
              'views/category_view.xml',
--- hr_disciplinary_tracking/docs/RELEASE_NOTES.md
+++ hr_disciplinary_tracking/docs/RELEASE_NOTES.md
@@ -1,6 +1,11 @@
 ## Module disciplinary_action
 
-#### 30.09.2021
+#### 03.09.2021
 #### Version 15.0.1.0.0
 ##### ADD
 - Initial Commit
+
+#### 03.05.2022
+#### Version 15.0.1.0.0
+#### FIX
+- Disciplinary Tracking Security 
