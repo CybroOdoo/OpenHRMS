bfc0f038919f25c61d041007c5502a24a22b13d1
Mario Arias Badila <the.clone.master@gmail.com>
2023-01-06 16:17:05 -0600

    Fix current company from env in multi company
    self.env.user_id.company_id returns the default company linked to current user, not the "Actual" company the user is working on. It is even possible that "default" company is not selected, triggering a security rule error.
    Correct use is self.env.company, that will provide the actual company the user has active.

--- hr_employee_transfer/models/hr_contract.py
+++ hr_employee_transfer/models/hr_contract.py
@@ -4,7 +4,7 @@ from odoo import models, fields, api
 class HrContract(models.Model):
     _inherit = 'hr.contract'
 
-    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.user.company_id)
+    company_id = fields.Many2one('res.company', 'Company', default=lambda self: self.env.company)
     from_transfer = fields.Boolean(string='Transferred', default=False)
     emp_transfer = fields.Many2one('employee.transfer', string='Transferred Employee', help="Transferred employee")
 
