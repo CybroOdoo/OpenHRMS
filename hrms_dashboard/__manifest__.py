15b6d68d91bd1ebcaf7f1e92571d30031d783104
AjmalCybro <ajmal@cybrosys.in>
2023-01-30 20:33:55 +0530

    Jan 30 [UPDT] : Style Updated 'hrms_dashboard'

--- hrms_dashboard/__manifest__.py
+++ hrms_dashboard/__manifest__.py
@@ -22,7 +22,7 @@
 ###################################################################################
 {
     'name': "Open HRMS - HR Dashboard",
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'summary': """Open HRMS - HR Dashboard""",
     'description': """Open HRMS - HR Dashboard""",
     'category': 'Generic Modules/Human Resources',
