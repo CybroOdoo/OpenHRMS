3ca067b794b4f117119ad8fd606473a7e1692feb
AjmalCybro <ajmal@cybrosys.in>
2023-07-20 12:41:38 +0530

    Jul 20 : [FIX] Bug Fixed 'hr_payroll_community'

--- hr_payroll_community/__manifest__.py
+++ hr_payroll_community/__manifest__.py
@@ -3,7 +3,7 @@
 {
     'name': 'Odoo16 Payroll',
     'category': 'Generic Modules/Human Resources',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.1',
     'author': 'Odoo SA,Cybrosys Techno Solutions',
     'company': 'Cybrosys Techno Solutions',
     'maintainer': 'Cybrosys Techno Solutions',
