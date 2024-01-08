bde5a7cb10193c5e89ee00b554268f654d051d61
AjmalCybro <ajmal@cybrosys.in>
2024-01-08 14:40:19 +0530

    Jan 8 [UPDT] : Updated 'oh_employee_documents_expiry'

--- oh_employee_documents_expiry/__manifest__.py
+++ oh_employee_documents_expiry/__manifest__.py
@@ -22,7 +22,7 @@
 ###################################################################################
 {
     'name': 'Open HRMS Employee Documents',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.0.2',
     'summary': """Manages Employee Documents With Expiry Notifications.""",
     'description': """OH Addon: Manages Employee Related Documents with Expiry Notifications.""",
     'live_test_url': 'https://youtu.be/4fe5tzAG8Ng',
--- oh_employee_documents_expiry/models/hr_documents.py
+++ oh_employee_documents_expiry/models/hr_documents.py
@@ -10,4 +10,3 @@ class HrDocument(models.Model):
     note = fields.Text(string='Note', copy=False, help="Note")
     attach_id = fields.Many2many('ir.attachment', 'attach_rel', 'doc_id', 'attach_id3', string="Attachment",
                                  help='You can attach the copy of your document', copy=False)
-
