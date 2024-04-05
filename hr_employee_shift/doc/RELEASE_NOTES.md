81c973be32cb6bcc8f7c1112d12d2453a1d89d73
RisvanaCybro <risvana@cybrosys.info>
2024-04-05 15:19:39 +0530

    Apr 05: [ADD] Initial Commit 'hr_employee_shift'

---
+++ hr_employee_shift/doc/RELEASE_NOTES.md
@@ -0,0 +1,6 @@
+## Module <hr_employee_shift>
+
+#### 28.03.2024
+#### Version 17.0.1.0.0
+##### ADD
+- Initial commit for Open HRMS Employee Shift
---
+++ hr_employee_shift/models/hr_contract.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#    A part of Open HRMS Project <https://www.openhrms.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2024-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+from odoo import fields, models
+
+
+class HrContract(models.Model):
+    _inherit = 'hr.contract'
+
+    shift_schedule = fields.One2many('hr.shift.schedule',
+                                     'rel_hr_schedule', string="Shift Schedule",
+                                     help="Shift schedule")
+    working_hours = fields.Many2one('resource.calendar',
+                                    string='Working Schedule', help="Working hours")
