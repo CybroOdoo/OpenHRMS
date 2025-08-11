f0914e853d85c22d7aaa924101a2edcc839437ac
AjmalCybro <ajmal@cybrosys.in>
2025-08-11 12:35:55 +0530

    Aug 11 [ADD] : Initial Commit 'hr_gratuity_settlement'

---
+++ hr_gratuity_settlement/__manifest__.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+#############################################################################
+#    A part of Open HRMS Project <https://www.openhrms.com>
+#
+#    Cybrosys Technologies Pvt. Ltd.
+#
+#    Copyright (C) 2025-TODAY Cybrosys Technologies(<https://www.cybrosys.com>)
+#    Author: Cybrosys Techno Solutions(<https://www.cybrosys.com>)
+#
+#    You can modify it under the terms of the GNU LESSER
+#    GENERAL PUBLIC LICENSE (LGPL v3), Version 3.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU LESSER GENERAL PUBLIC LICENSE (LGPL v3) for more details.
+#
+#    You should have received a copy of the GNU LESSER GENERAL PUBLIC LICENSE
+#    (LGPL v3) along with this program.
+#    If not, see <http://www.gnu.org/licenses/>.
+#
+#############################################################################
+{
+    'name': 'Open HRMS Gratuity Settlement',
+    'version': '18.0.1.0.0',
+    'category': 'Human Resources',
+    'summary': """Employee Gratuity Settlement""",
+    'description': """This module provides an option to create Gratuity 
+    Settlements for Employees During Resignation""",
+    'author': 'Cybrosys Techno solutions, Open HRMS',
+    'company': 'Cybrosys Techno Solutions',
+    'maintainer': 'Cybrosys Techno Solutions',
+    'website': 'https://www.cybrosys.com',
+    'depends': ['hr_payroll_community', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'data/ir_sequence_data.xml',
+        'views/gratuity_configuration_views.xml',
+        'views/hr_contract_views.xml',
+        'views/hr_gratuity_accounting_configuration_views.xml',
+        'views/hr_gratuity_views.xml',
+        'views/hr_training_views.xml',
+    ],
+    'images': ['static/description/banner.jpg'],
+    'license': 'LGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
---
+++ hr_gratuity_settlement/doc/RELEASE_NOTES.md
@@ -0,0 +1,7 @@
+## Module <hr_gratuity_settlement>
+
+#### 01.08.2025
+#### Version 18.0.1.0.0
+#### ADD
+
+- Initial commit for Open HRMS Gratuity Settlement
